a3932f8fe830f9e597f142f3e2be6ca9c6af1933
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-02-23 12:02:56 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+andrewrothstein.sonobuoy
+=========
+[![Build Status](https://travis-ci.org/andrewrothstein/ansible-sonobuoy.svg?branch=master)](https://travis-ci.org/andrewrothstein/ansible-sonobuoy)
+
+Installs [sonobuoy](https://sonobuoy.io/).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.sonobuoy
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+set -e
+DIR=~/Downloads
+APP=sonobuoy
+MIRROR=https://github.com/vmware-tanzu/${APP}/releases/download
+
+dl() {
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="${APP}_${ver}_${platform}.${archive_type}"
+    local url="$MIRROR/v$ver/$file"
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    local url="$MIRROR/v$ver/${APP}_${ver}_checksums.txt"
+    local lchecksums="$DIR/${APP}_${ver}_checksums.txt"
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums linux 386
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm64
+    dl $ver $lchecksums windows 386
+    dl $ver $lchecksums windows amd64
+}
+
+dl_ver ${1:-0.17.2}
