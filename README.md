6ab6858b1efde8f64c90e7adcbdc539b129c9b50
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-11-21 21:24:39 -0500

    sonobuoy:0.56.12, travis -> github actions, latest CIs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 andrewrothstein.sonobuoy
 =========
-[![Build Status](https://travis-ci.org/andrewrothstein/ansible-sonobuoy.svg?branch=master)](https://travis-ci.org/andrewrothstein/ansible-sonobuoy)
+![Build Status](https://github.com/andrewrothstein/ansible-sonobuoy/actions/workflows/build.yml/badge.svg)
 
 Installs [sonobuoy](https://sonobuoy.io/).
 
--- dl-checksums.sh
+++ dl-checksums.sh
@@ -23,18 +23,21 @@ dl_ver() {
     local lchecksums="$DIR/${APP}_${ver}_checksums.txt"
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $url
+        curl -sSLf -o $lchecksums $url
     fi
 
     printf "  # %s\n" $url
     printf "  '%s':\n" $ver
 
     dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums darwin arm64
     dl $ver $lchecksums linux 386
     dl $ver $lchecksums linux amd64
     dl $ver $lchecksums linux arm64
+    dl $ver $lchecksums linux ppc64le
+    dl $ver $lchecksums linux s390x
     dl $ver $lchecksums windows 386
     dl $ver $lchecksums windows amd64
 }
 
-dl_ver ${1:-0.17.2}
+dl_ver ${1:-0.56.12}
